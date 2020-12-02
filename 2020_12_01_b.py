cb9f80d19bb6fb5ebce4c9b337b723d0821ef991
catanddogsoup <catanddogsoup@gmail.com>
2020-12-02 22:29:01 +0000

    a

---
+++ 2020_12_01_b.py
@@ -0,0 +1,15 @@
+with open("2020_12_01_list", "r") as f:
+    read_data = f.read()
+    split_data = read_data.split("\n")
+    split_data = list(filter(None, split_data))
+    for i in range(0, len(split_data)):
+        split_data[i] = int(split_data[i])
+    #print(split_data)
+    for i in split_data:
+        for j in split_data:
+            for k in split_data:
+                #print(i + j)
+                l = i+j+k
+                if l == 2020:
+                    #print(i, '  ', j, '  ', k)
+                    print(i*j*k)
