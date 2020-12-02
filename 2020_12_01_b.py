cddf14ba90f1818ae29f6bb2a70be512b15f43cb
catanddogsoup <catanddogsoup@gmail.com>
2020-12-02 23:05:28 +0000

    comments and started day 2

--- 2020_12_01_b.py
+++ 2020_12_01_b.py
@@ -1,10 +1,15 @@
+# Read file with AdventOfCode day 1 list
 with open("2020_12_01_list", "r") as f:
     read_data = f.read()
+    # Split data into array
     split_data = read_data.split("\n")
+    # Remove any empty listings
     split_data = list(filter(None, split_data))
+    # Convert all array items to ints
     for i in range(0, len(split_data)):
         split_data[i] = int(split_data[i])
-    #print(split_data)
+    # Run through each item (j) for every item (i) and every item (k) and compares if they add up to 2020
+    # and prints the result if it does
     for i in split_data:
         for j in split_data:
             for k in split_data:
---
+++ 2020_12_02_a.py
@@ -0,0 +1,2 @@
+with open("2020_12_02_list", "r") as f:
+    read_data = f.read()
