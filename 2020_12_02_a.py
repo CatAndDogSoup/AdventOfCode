fe1acb7cb7defb4e32e533b0d355e409218eeaa8
catanddogsoup <catanddogsoup@gmail.com>
2020-12-03 00:22:22 +0000

    comments and started day 2

--- 2020_12_02_a.py
+++ 2020_12_02_a.py
@@ -1,2 +1,25 @@
+# Read file with AdventOfCode day 2 list
 with open("2020_12_02_list", "r") as f:
     read_data = f.read()
+    # Split data into array
+    split_data = read_data.split("\n")
+    # Remove any empty listings
+    split_data = list(filter(None, split_data))
+    amount = 0
+    for item in split_data:
+        item_array = tuple(item.split(" "))
+        #print(item_array)
+        item_minmax = item_array[0].split("-")
+        item_min = item_minmax[0]
+        item_max = item_minmax[1]
+        #print(item_min)
+        #print(item_max)
+        item_letter = item_array[1]
+        item_password = item_array[2]
+        #print(item_letter)
+        #print(item_password)
+        item_letter = item_letter.replace(':','')
+        if item.count(item_letter) > int(item_min) and item.count(item_letter) < int(item_max):
+            #print(item)
+            amount = amount + 1
+    print(amount)
