dd9b5d3987e79b3c1a0c74204e90f3d5c47fcfd2
catanddogsoup <catanddogsoup@gmail.com>
2020-12-03 00:51:43 +0000

    comments

---
+++ 2020_12_02_b.py
@@ -0,0 +1,24 @@
+# Read file with AdventOfCode day 2 list
+with open("2020_12_02_list", "r") as f:
+    read_data = f.read()
+    # Split data into array
+    split_data = read_data.split("\n")
+    # Remove any empty listings
+    split_data = list(filter(None, split_data))
+    amount = 0
+    #loop over every item in #split_data
+    for item in split_data:
+        #split item into item_array based on spaces
+        item_array = tuple(item.split(" "))
+        #split item_array part 0 (min-max) into seperate variables
+        item_minmax = item_array[0].split("-")
+        item_min = item_minmax[0]
+        item_max = item_minmax[1]
+        #sets the item_letter as the item to check how many of each are in the password
+        item_letter = item_array[1].replace(':','')
+        #sets item_password as the password
+        item_password = item_array[2]
+        #checks if the item_letter is in the password on item_min and item_max positions
+        if item_password[int(item_min)-1] == item_letter is not item_password[int(item_max)-1] == item_letter:
+            amount = amount + 1
+    print(amount)
