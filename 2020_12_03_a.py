eed495a71c412a8451d42e9c3e414dcbcddb1507
catanddogsoup <catanddogsoup@gmail.com>
2020-12-04 00:13:50 +0000

    comments

--- 2020_12_03_a.py
+++ 2020_12_03_a.py
@@ -9,19 +9,22 @@ with open("2020_12_03_list", "r") as f:
     width = len(split_data[0])
     # x_pos for the current horizontal position
     x_pos = 0
+    x_move = 3
+    y_move = 1
     # amount for the final amount of trees hit
     amount = 0
+
     # loop over every line in split_data
-    for i in range(0, len(split_data)):
+    for i in range(0, len(split_data), y_move):
         # up x_pos for every iteration
-        x_pos = x_pos + 3
-        # wrap x_pos around
-        if x_pos >= width:
-            x_pos = x_pos - width
 
         if split_data[i][x_pos] == "#":
             print(split_data[i][:x_pos] + "X" + split_data[i][x_pos + 1:] + " | " + str(x_pos) + "\t| " + str(i))
             amount = amount + 1
         else:
             print(split_data[i])
+        x_pos = x_pos + x_move
+        # wrap x_pos around
+        if x_pos >= width:
+            x_pos = x_pos - width
     print(amount)
