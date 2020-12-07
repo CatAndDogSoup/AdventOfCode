638dc1012034c3b146d2dc90e099490751e55bb5
catanddogsoup <catanddogsoup@gmail.com>
2020-12-07 22:38:13 +0000

    K

--- 2020_12_04_b.py
+++ 2020_12_04_b.py
@@ -1,5 +1,6 @@
 # Read file with AdventOfCode day 4 list
 import re
+import string
 with open("2020_12_04_list", "r") as f:
     read_data = f.read()
     # Split data into array
@@ -12,6 +13,8 @@ with open("2020_12_04_list", "r") as f:
     required_data = set(["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"])
     amount = 0
     ecl = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+
     #print(split_data)
     for item in split_data:
         flattened = item.replace('\n', ' ')
@@ -23,22 +26,19 @@ with open("2020_12_04_list", "r") as f:
                 value = i.split(':')[1]
                 item_dict[key]=value
         if required_data.issubset(item_dict.keys()):
-            print(item_dict)
+            #print(item_dict)
             #print(item_dict["pid"])
             if 1920 <= int(item_dict["byr"]) <= 2002:
                 if 2010 <= int(item_dict["iyr"]) <= 2020:
                     if 2020 <= int(item_dict["eyr"]) <= 2030:
-                        if "#" in item_dict["hcl"] and len(item_dict["hcl"]) == 7 and re.compile(r'[^a-f0-9]').search:
+                        if "#" in item_dict["hcl"][0] and len(item_dict["hcl"]) == 7 and re.compile(r'[^a-f0-9]').search:
                             if item_dict["ecl"] in ecl:
                                 if len(item_dict["pid"]) == 9:
                                     if "in" in item_dict["hgt"]:
-                                        #print("poggies")
-                                        amount = amount + 1
-                                    if "cm" in item_dict["hgt"]:
-                                        #print("poggies")
-                                        amount = amount + 1
-
-                        #print(item_dict)
-
+                                        if 59 <= int(re.sub("\D", "", item_dict["hgt"])) >= 76:
+                                            amount = amount + 1
+                                    elif "cm" in item_dict["hgt"]:
+                                        if 150 <= int(re.sub("\D", "", item_dict["hgt"])) >= 193:
+                                            amount = amount + 1
 
     print(amount)
